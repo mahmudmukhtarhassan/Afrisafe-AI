008055fca711d01f8e5e191fd09524e8f990fbbe
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-21 22:38:11 +0100

    Update user.py

--- backend/app/user.py
+++ backend/app/user.py
@@ -20,32 +20,5 @@ class UserLogin(BaseModel):
 class Token(BaseModel):
 
     access_token: str
-
-    from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
-
-from app.database import Base
-
-
-class User(Base):
-
-    __tablename__ = "users"
-
-    id = Column(
-        Integer,
-        primary_key=True,
-        index=True
-    )
-
-    fullname = Column(String)
-
-    email = Column(
-        String,
-        unique=True,
-        index=True
-    )
-
-    password = Column(String)
-
-    token_type: str
+    
+    
