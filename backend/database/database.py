17c0328e5772968f1a7a6c2a0862be926f79b5ae
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-21 22:17:53 +0100

    Update database.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -24,4 +24,30 @@ def get_db():
         yield db
     finally:
         db.close()
-users = []
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+
+DATABASE_URL = "sqlite:///./afrisafe.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
