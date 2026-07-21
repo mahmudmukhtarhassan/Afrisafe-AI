4451665ab234d620f46e0d116dc41f4c0423a5eb
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-21 22:44:42 +0100

    Update router.py

--- backend/auth/router.py
+++ backend/auth/router.py
@@ -1,1 +1,65 @@
+@router.post("/register")
+def register(
+    user: UserRegister,
+    db: Session = Depends(get_db)
+):
 
+    existing = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists"
+        )
+
+    new_user = User(
+        fullname=user.fullname,
+        email=user.email,
+        password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+
+    return {
+        "message":"Registration successful"
+    }
+@router.post("/login")
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    existing = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing is None:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    if not verify_password(
+        user.password,
+        existing.password
+    ):
+
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    token = create_access_token(
+        {
+            "sub": existing.email
+        }
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
