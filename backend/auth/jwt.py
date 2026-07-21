65877d52502f2313ab3bb98844a7d6d1e9486407
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-21 21:49:21 +0100

    Update jwt.py

--- backend/auth/jwt.py
+++ backend/auth/jwt.py
@@ -1,1 +1,31 @@
+from jose import jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_EXPIRE = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+)
+
+
+def create_access_token(data: dict):
+
+    payload = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_EXPIRE
+    )
+
+    payload.update({"exp": expire})
+
+    token = jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return token
