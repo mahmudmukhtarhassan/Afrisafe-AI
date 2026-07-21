d05579b5e9cada0587d4ed193e640aa22bfdcd76
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-21 21:34:04 +0100

    Update database.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,1 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
+DATABASE_URL = "sqlite:///./afrisafe.db"
+# Later we'll change this to PostgreSQL.
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
