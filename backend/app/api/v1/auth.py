78092a7ff11191ed1d59c4cf50eda3a0ecf8ae92
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-28 00:27:23 +0100

    Create auth.py

---
+++ backend/app/api/v1/auth.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter, HTTPException, Header
+
+from app.schemas.auth import RegisterRequest, LoginRequest
+
+from app.services.auth_service import (
+    register_user,
+    login_user,
+    logout_user,
+)
+
+router = APIRouter(
+    prefix="/api/v1/auth",
+    tags=["Authentication"],
+)
+
+
+@router.post("/register")
+async def register(data: RegisterRequest):
+
+    try:
+
+        result = await register_user(
+            data.full_name,
+            data.email,
+            data.password,
+        )
+
+        return {
+            "message": "Registration successful",
+            "user": result.user
+        }
+
+    except Exception as e:
+        raise HTTPException(400, str(e))
+
+
+@router.post("/login")
+async def login(data: LoginRequest):
+
+    try:
+
+        session = await login_user(
+            data.email,
+            data.password,
+        )
+
+        return {
+            "access_token": session.session.access_token,
+            "refresh_token": session.session.refresh_token,
+            "user": session.user,
+        }
+
+    except Exception as e:
+        raise HTTPException(401, str(e))
+
+
+@router.post("/logout")
+async def logout(authorization: str = Header()):
+
+    token = authorization.replace("Bearer ", "")
+
+    await logout_user(token)
+
+    return {"message": "Logged out"}
