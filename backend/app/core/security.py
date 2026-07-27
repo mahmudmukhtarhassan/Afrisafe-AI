ac2487246b97cc295930f31a6c52df9a9c7d1aee
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-28 00:32:20 +0100

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,22 +1,19 @@
-from datetime import datetime, timedelta
-
-from jose import jwt
-
+from jose import jwt, JWTError
+from fastapi import HTTPException, status
 from app.core.config import settings
 
 
-def create_access_token(data: dict):
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
+        )
+        return payload
 
-    payload = data.copy()
-
-    expire = datetime.utcnow() + timedelta(
-        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-    )
-
-    payload.update({"exp": expire})
-
-    return jwt.encode(
-        payload,
-        settings.SECRET_KEY,
-        algorithm=settings.ALGORITHM,
-    )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
