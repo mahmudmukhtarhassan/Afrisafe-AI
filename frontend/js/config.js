39bff018dec987c086c4cd5d0bbf8a7f73c47be1
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-29 22:02:54 +0100

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,7 +5,7 @@
  */
 
 // Local Express backend API base URL
-const API_BASE_URL = "";
+const API_BASE_URL = "http://127.0.0.1:8000";
 
 // ---------------------------------------------------------------------------
 // Token Storage Helpers
